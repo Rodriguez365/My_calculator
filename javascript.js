b86ffda35847b51ca0cd64e0c76d64df7fa8924b
Rodriguez365 <rmwila61@gmail.com>
2023-01-17 05:15:10 -0700

    My own calculator algarithm

--- javascript.js
+++ javascript.js
@@ -1,50 +1,138 @@
-let equation = "Syntax Error";
+let split_list = "";
+let first_Num = 0;
+let second_num = 0;
+let answer1 = [];
+let answer_text = document.querySelector("p");
+let last_num_index = 0;
+let text = document.getElementById("text1").value;
 
 function calculate() {
     let text = document.getElementById("text1").value;
-    equation = eval(text);
-    return console.log(equation);
+    if (operator == "*" && operator_counter == 1) {
+        split_list = text.split("*");
+        first_Num = Number(split_list[0]);
+        second_num = Number(split_list[1]);
+        answer1 = first_Num * second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "*" && operator_counter >= 2) {
+        split_list = text.split("*")
+        first_Num = Number(answer1);
+        last_num_index = split_list.length - 1;
+        second_num = Number(split_list[last_num_index]);
+        answer1 = first_Num * second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "+" && operator_counter == 1) {
+        split_list = text.split("+");
+        first_Num = Number(split_list[0]);
+        second_num = Number(split_list[1]);
+        answer1 = first_Num + second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "+" && operator_counter >= 2) {
+        split_list = text.split("+")
+        first_Num = Number(answer1);
+        last_num_index = split_list.length - 1;
+        second_num = Number(split_list[last_num_index]);
+        answer1 = first_Num + second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "/" && operator_counter == 1) {
+        split_list = text.split("/");
+        first_Num = Number(split_list[0]);
+        second_num = Number(split_list[1]);
+        answer1 = first_Num / second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "/" && operator_counter >= 2) {
+        split_list = text.split("/")
+        first_Num = Number(answer1);
+        last_num_index = split_list.length - 1;
+        second_num = Number(split_list[last_num_index]);
+        answer1 = first_Num / second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "-" && operator_counter == 1) {
+        split_list = text.split("-");
+        first_Num = Number(split_list[0]);
+        second_num = Number(split_list[1]);
+        answer1 = first_Num - second_num;
+        answer_text.textContent = answer1;
+
+    } else if (operator == "-" && operator_counter >= 2) {
+        split_list = text.split("-")
+        first_Num = Number(answer1);
+        last_num_index = split_list.length - 1;
+        second_num = Number(split_list[last_num_index]);
+        answer1 = first_Num - second_num;
+        answer_text.textContent = answer1;
+
+
+    }
+
 }
+
+
+
 let calculate_text = document.getElementById("text1");
-let answer = document.getElementById("answer1");
 const buttons = document.querySelectorAll("button");
-let plus = 0;
+let equal_sign_counter = 0;
+let operator = "";
+let operator_counter = 0;
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        if (button.id == "c") {
+        if (button.id == "+") {
+            operator = button.id;
+            operator_counter += 1;
+            calculate_text.value += button.id;
+        } else if (button.id == "/") {
+            operator_counter += 1;
+            operator = button.id;
+            calculate_text.value += button.id;
+        } else if (button.id == "-") {
+            operator_counter += 1;
+            operator = button.id;
+            calculate_text.value += button.id;
+        } else if (button.id == "*") {
+            operator_counter += 1;
+            operator = button.id;
+            calculate_text.value += button.id;
+        } else if (button.id == "c") {
             calculate_text.value = "";
-            answer.value = ""
-            plus = 0;
-            equation = "Syntax Error"
+            answer_text.textContent = ""
+            equal_sign_counter = 0;
+            operator = "";
+            answer1 = 0;
+            operator_counter = 0;
+
         } else if (button.id == "equal_btn") {
-            plus += 1;
-            if (plus > 1){
-                answer.value = equation
-                equation = "Syntax Error";
+            equal_sign_counter += 1;
+            if (plus > 1) {
+                operator = "";
+                answer1 = 0;
+                operator_counter = 0;
             } else {
-            calculate_text.value += "="
-            answer.value = equation;
-            equation = "Syntax Error";
-            }   
-        } else if (button.id == "step") {
-            let remove = calculate_text.value.slice(0, -1);
-            calculate_text.value = remove;
-            answer.value = "";
-            plus = 0;
-            equation = "Syntax Error"
-        } else {
-            if (plus >= 1){
-              answer.value = ""
-              calculate_text.value = ""
-              calculate_text.value += button.id;  
-              plus = 0;
-              equation = "Syntax Error";
-            } else {
-                answer.value = ""
-                calculate_text.value += button.id;  
+                calculate_text.value += "="
+                operator = "";
+                answer1 = 0;
+                operator_counter = 0;
             }
-                
-                }
+        } else {
+            if (equal_sign_counter >= 1) {
+                answer_text.textContent = ""
+                calculate_text.value = ""
+                calculate_text.value += button.id;
+                equal_sign_counter = 0;
+                answer1 = 0;
+
+            } else {
+                answer_text.textContent = ""
+                calculate_text.value += button.id;
+                calculate()
+            }
+
+        }
 
 
     })

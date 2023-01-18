e95fa30a988820d740174c87e8909028514acfaa
Rodriguez365 <rmwila61@gmail.com>
2023-01-18 02:19:39 -0700

    added a few calculator like designs

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let split_list = "";
 let first_Num = 0;
 let second_num = 0;
 let answer1 = [];
-let answer2 = 0;
+let rounded_division = 0;
 let answer_text = document.querySelector("p");
 let last_num_index = 0;
 let text = document.getElementById("text1").value;
@@ -44,8 +44,8 @@ function calculate() {
         first_Num = Number(split_list[0]);
         second_num = Number(split_list[1]);
         answer1 = first_Num / second_num;
-        answer2 = answer1.toFixed(6)
-        answer_text.textContent = answer2;
+        rounded_division = answer1.toFixed(6)
+        answer_text.textContent = rounded_division;
 
     } else if (operator == "/" && operator_counter >= 2) {
         split_list = text.split("/")
@@ -53,8 +53,8 @@ function calculate() {
         last_num_index = split_list.length - 1;
         second_num = Number(split_list[last_num_index]);
         answer1 = first_Num / second_num;
-        answer2 = answer1.toFixed(6)
-        answer_text.textContent = answer2;
+        rounded_division = answer1.toFixed(6)
+        answer_text.textContent = rounded_division;
 
     } else if (operator == "-" && operator_counter == 1) {
         split_list = text.split("-");
@@ -110,30 +110,31 @@ buttons.forEach(button => {
             operator_counter = 0;
 
         } else if (button.id == "equal_btn") {
-            equal_sign_counter += 1
+            equal_sign_counter += 1;
             if (equal_sign_counter > 1) {
-                answer_text.textContent = answer1
-                calculate_text.value += ""
+                operator = "";
+                answer1 = 0;
                 operator_counter = 0;
             } else {
-                answer_text.textContent = answer1
-                calculate_text.value += "=";
+                calculate_text.value += "="
+                operator = "";
+                answer1 = 0;
                 operator_counter = 0;
-
-
             }
-        } else if (equal_sign_counter >= 1 && operator_counter < 1) {
-            answer_text.textContent = ""
-            calculate_text.value = "";
-            operator = "";
-            calculate_text.value += button.id;
-            operator_counter = 0;
-            equal_sign_counter = 0;
-            calculate()
         } else {
-            answer_text.textContent = ""
-            calculate_text.value += button.id;
-            calculate()
+            if (equal_sign_counter >= 1) {
+                answer_text.textContent = ""
+                calculate_text.value = ""
+                calculate_text.value += button.id;
+                equal_sign_counter = 0;
+                answer1 = 0;
+
+            } else {
+                answer_text.textContent = ""
+                calculate_text.value += button.id;
+                calculate()
+            }
+
         }
 
 
